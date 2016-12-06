851a365c9eeff714d01a25fc6f32db5d9b114b01
Sergei Turukin <rampage644@gmail.com>
2016-12-06 11:56:17 +0300

    Basic train routine

---
+++ train.py
@@ -0,0 +1,20 @@
+'''Train the model'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import tensorflow as tf
+
+
+FLAGS = tf.app.flags.FLAGS
+
+
+def main(argv=None):
+    '''main entry point'''
+    pass
+
+
+if __name__ == '__main__':
+    tf.app.run()
+
