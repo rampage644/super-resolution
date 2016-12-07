7858599ff19f0a2a6f4e959d9c9b672a05005307
Sergei Turukin <rampage644@gmail.com>
2016-12-07 18:06:07 +0300

    Load model for image generation

--- test.py
+++ test.py
@@ -17,6 +17,7 @@ import subpixel.util
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('log_dir', os.path.join(os.getcwd(), 'log'), 'Directory with training data (images)')
+tf.app.flags.DEFINE_string('ckpt_dir', os.path.join(os.getcwd(), 'ckpt'), 'Directory for model checkpoints')
 tf.app.flags.DEFINE_integer('factor', 3, 'Upscaling factor')
 tf.app.flags.DEFINE_string('input', '', 'Path to image to upscale')
 tf.app.flags.DEFINE_string('output', 'output.png', 'Path to output image')
@@ -29,15 +30,27 @@ KERNEL_SIZES = [5, 3, 3]
 def main(argv=None):
     '''main entry point'''
     with tf.Session() as sess:
+        image = scipy.misc.imread(FLAGS.input)
+
         FLAGS.filters = FILTERS + [FLAGS.factor ** 2 * COLORS]
         FLAGS.kernel_sizes = KERNEL_SIZES
         FLAGS.strides = [1] * len(KERNEL_SIZES)
+        FLAGS.height, FLAGS.width, _ = image.shape
+        FLAGS.learning_rate = 0.1
 
         model = subpixel.model.SuperResolution(FLAGS)
 
         sess.run(tf.global_variables_initializer())
 
-        image = scipy.misc.imread(FLAGS.input)
+        saver = tf.train.Saver()
+        saved_model = tf.train.latest_checkpoint(FLAGS.ckpt_dir)
+        if saved_model:
+            saver.restore(sess, saved_model)
+        else:
+            print('No saved model found, exiting.')
+            return 1
+
+
         upscaled = sess.run(model.predicted, {
             model.input: np.expand_dims(image, axis=0)
         })
